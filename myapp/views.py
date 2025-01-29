358d931760d744f17a86a65d2ad02afed1cb46e7
mahruf60 <mahruf9060@gmail.com>
2025-01-30 01:52:18 +0600

    Categorical expenses

--- myapp/views.py
+++ myapp/views.py
@@ -31,7 +31,8 @@ def index(request):
     weekly_sum = data.aggregate(Sum('amount'))
 
     daily_sums = Expense.objects.filter().values('date').order_by('date').annotate(sum=Sum('amount'))
-    
+    categorical_sums = Expense.objects.filter().values('category').order_by('category').annotate(sum=Sum('amount'))
+
     context = {
         'expense_form':expense_form,
         'expenses':expenses,
@@ -40,6 +41,7 @@ def index(request):
         'monthly_sum':monthly_sum,
         'weekly_sum':weekly_sum,
         'daily_sums':daily_sums,
+        'categorical_sums':categorical_sums,
     }
     return render(request,'myapp/index.html',context)
 
